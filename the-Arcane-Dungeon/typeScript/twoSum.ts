1eef58bea322404d6c9ec2e258ad95119c7e2ccb
Md Rezaul Hossain <str.rezaul@gmail.com>
2025-05-13 23:34:18 +0600

    two sum in O(n)

---
+++ the-Arcane-Dungeon/typeScript/twoSum.ts
@@ -0,0 +1,20 @@
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+function twoSum(nums: number[], target: number): number[] {
+    let remaining: Map<number, number> = new Map();
+
+    for(let i=0; i<nums.length; i++){
+        let diff:number = target-nums[i];
+        if(remaining.has(diff)){
+            return [remaining.get(diff)!, i];
+        } else{
+            remaining.set(nums[i], i);
+        }
+    }
+
+    return [];
+
+};
