20f530144846bbfc00ddb8c782b912510d4d596d
Md Rezaul Hossain <str.rezaul@gmail.com>
2025-05-13 22:22:32 +0600

    Maps methods

---
+++ the-Grimoire-of-Algo/maps/maps.ts
@@ -0,0 +1,8 @@
+let newMap: Map<number, string> = new Map();
+
+newMap.set(1, "one");
+newMap.set(2, "one");
+newMap.set(3, "one");
+
+console.log(newMap);
+console.log(newMap.has(2)); 
---
+++ the-Grimoire-of-Algo/maps/maps_methods.js
@@ -0,0 +1,16 @@
+let myMap = new Map();
+
+myMap.set(1, "Uno");
+myMap.set(2, "Dos");
+myMap.set(3, "Tres");
+myMap.set(4, "Cuatro");
+
+console.log(myMap.get(3));
+
+console.log(myMap.has(6));
+
+console.log(myMap.delete(1));
+
+console.log(myMap);
+
+console.log(myMap.size);
